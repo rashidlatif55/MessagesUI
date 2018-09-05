a5a50a4fd914f35e62a88ae8f049e6ec836bea3f
Rashid Latif <rashid.latif93@gmail.com>
2018-09-05 15:56:00 +0500

    Initial Commit

---
+++ Message/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Message
+//
+//  Created by Rashid on 05/09/2018.
+//  Copyright © 2018 GlobizServe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
